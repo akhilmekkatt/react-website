d6ac4da1ec0affd2ee39c200179e9bc323279b44
Akhil Mekkatt <akhilmekkatt@gmail.com>
2020-05-20 13:16:14 +0530

    content added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Under construction
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi I Am AKHIL</h1>
+      <p>Website under Construction</p>
+      <p>Email:akhilmekkatt@gmail.com</p>
     </div>
   );
 }
